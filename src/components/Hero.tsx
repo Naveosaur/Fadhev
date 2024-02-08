be279cecce824f9b467536cbedc1b8d39aa2fb22
Evan Fadhilla <evfadhw@gmail.com>
2024-02-08 09:14:45 +0700

    update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,9 +14,7 @@ const Hero = () => {
         <div className="flex flex-col m-5 p-5 md:w-[60vh] ">
           <h1 className="text-2xl font-bold md:text-4xl">Evan Fadhilla W</h1>
           <h2 className="text-xl font-medium text-gray-600 dark:text-gray-400 -my-1 mb-2">Full-stack Web Developer</h2>
-          <p className="text-gray-900 dark:text-gray-400 md:text-lg">
-            Full-Stack Web Developer with three years of experience in web development, UI design, and product management specializing in building exceptional digital experiences for your business needs.{" "}
-          </p>
+          <p className="text-gray-900 dark:text-gray-400 md:text-lg">Full-Stack Web Developer, UI design, and product management specializing in building exceptional digital experiences for your business needs. </p>
           <div className="mt-10 flex gap-5">
             <Button className="rounded-full">
               <Link href="#works" className=" text-xl font-bold flex items-center">
